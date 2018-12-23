20a5aa4382da87d25ad947187bedec2ff11c771f
LeoCantThinkOfAName <i7851@hotmail.com>
2018-12-23 20:19:27 +0800

    Updates

---
+++ precache-manifest.c8a3afdba70cf15ee851bdad18e096fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22af8ff1419ab3b085608e48ae299d1e",
+    "url": "/react-tmdb-searcher/static/media/tmdb.22af8ff1.svg"
+  },
+  {
+    "revision": "380511816dcb72fd6e89",
+    "url": "/react-tmdb-searcher/static/js/runtime~main.38051181.js"
+  },
+  {
+    "revision": "8b324c967908cdcb8b0a",
+    "url": "/react-tmdb-searcher/static/js/main.8b324c96.chunk.js"
+  },
+  {
+    "revision": "3438ea399ffeb042e517",
+    "url": "/react-tmdb-searcher/static/js/1.3438ea39.chunk.js"
+  },
+  {
+    "revision": "8b324c967908cdcb8b0a",
+    "url": "/react-tmdb-searcher/static/css/main.b5f3afa3.chunk.css"
+  },
+  {
+    "revision": "abee1b11dd3497f8e46c2784c2bee8a5",
+    "url": "/react-tmdb-searcher/index.html"
+  }
+];
