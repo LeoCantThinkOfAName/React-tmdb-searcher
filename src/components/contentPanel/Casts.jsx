da725b2a7afadd78403220302efc3e5dd6e58cee
LeoCantThinkOfAName <i7851@hotmail.com>
2018-12-23 19:39:03 +0800

    restructure folders' path

---
+++ src/components/contentPanel/Casts.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import placeholder from "../../images/profile-placeholder.png";
+
+export default function Casts({ casts }) {
+  if (casts.length === 0) {
+    return <span>...</span>;
+  }
+
+  return (
+    <ul className="castlist">
+      {casts.map(cast => (
+        <li key={cast.cast_id}>
+          <figure>
+            <img
+              src={
+                cast.profile_path != null
+                  ? `${process.env.REACT_APP_IMG}/w185${cast.profile_path}`
+                  : placeholder
+              }
+              alt={cast.name}
+            />
+            <figcaption>
+              <strong>{cast.name}</strong> as <strong>{cast.character}</strong>
+            </figcaption>
+          </figure>
+        </li>
+      ))}
+    </ul>
+  );
+}
